2ef90ec3cfa7adf19a11a277d6139304f78870c2
meriem <meriemismahlarbi@gmail.com>
2018-10-02 21:05:01 +0200

    nested component globally | locally

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <h1>{{title}}</h1>
+  <!--global nesting-->
+  <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'
+
+export default {
+  components:{
+    'ninjas':Ninjas,
+  },
+  data () {
+    return {
+      title:"Ninja App",
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color:purple;
+}
+</style>
